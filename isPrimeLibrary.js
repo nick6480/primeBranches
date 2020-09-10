d81dd5fadc7fe126ed985734afa9e93e1ee5d032
Nickolaj <nickolaj99@outlook.com>
2020-09-10 19:06:37 +0200

    test

--- isPrimeLibrary.js
+++ isPrimeLibrary.js
@@ -1,5 +1,5 @@
 const isPrime = function (arg) {
-    for (var i = 2; i < arg; i += 1) {
+    for (var i = 3; i < Math.sqrt(arg); i += 2) {
         if (arg % i === 0) {
             return false;
         }
